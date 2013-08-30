e3fe223902e8db410ba256d27ea779a9436acbe2
Niranjan B Prithviraj <nprithvi@yahoo-inc.com>
2013-08-30 23:48:33 +0530

    adding new layout and css

---
+++ index_new.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="Niranjan B Prithviraj's personal website. Find my contact information here.">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+<META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.2.0/pure-min.css">
+<link rel="stylesheet" href="css/stylenew.css">
+<style>
+@media (max-width: 480px) {
+  #content{width:80%;margin:auto;}
+}
+</style>
+<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="font-awesome/css/font-awesome-ie7.min.css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-29843668-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+<body>
+<div id="page">
+  <div id="content" class="pure-g-r">
+    <div id="header" class="pure-u-1-1">
+      <h1>My name is Niranjan B Prithviraj</h1>
+      <p>
+        I work at Yahoo!. I work with front end technologies and make the web more beautiful, responsive and easy to use. 
+      </p>
+    </div>
+    <div class="aboutme">
+      <ul>
+        <li class="icon"><i class="icon-facebook"></i></li>
+        <li class="icon"><i class="icon-skype"></i></li>
+        <li class="icon"><i class="icon-twitter"></i></li>
+        <li class="icon"><i class="icon-github"></i></li>
+        <li class="icon"><i class="icon-linkedin"></i></li>
+        <li class="icon"><i class="icon-flickr"></i></li>
+      </ul>
+    </div>
+  </div>
+  <footer>
+    akljsdlakj
+  </footer>
+</div>
+</body>
+</html>
